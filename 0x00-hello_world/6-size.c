9797e0c675fa884dce430d3be5e7de363515466f
am-afreemind <jmutebi001@gmail.com>
2022-09-11 08:42:45 -0700

    a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
  *This programs print the size of various
  *types compiled and run on
  *
-**And it is to return a 0 as output
+ *And it is to return a 0 as output
  *Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
@@ -17,7 +17,4 @@ int main(void)
 		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 		printf("Size of a float: %d byte(s)\n", sizeof(float));
 		return (0);
-}		
-
-
-							
+}									
