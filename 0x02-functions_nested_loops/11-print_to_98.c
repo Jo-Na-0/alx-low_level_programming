a149d46acf6691558912eb216dbb537b0f2aa277
am-afreemind <jmutebi001@gmail.com>
2022-09-15 16:36:02 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,35 @@
 
 #include <stdio.h>
 
-
-
 /**
 * print_to_98 - print numbers n to 98
+*
+* followed by a new line
 *
 * @n: integer argument
 */
 
 void print_to_98(int n)
 {
+	int i, j;
+
+	if (n <= 98)
 	{
-		printf("%d, ", n);
-		n++;
-	}
-	while (n > 98)
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	else if (n >= 98)
 	{
-		printf("%d, ", n);
-		n--;
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
-	if (n == 98)
-		printf("%d", n);
-	printf("\n");
 }
