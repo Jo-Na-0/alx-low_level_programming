c7d37cd161edf8450d9af250fde48b68621d0d40
am-afreemind <jmutebi001@gmail.com>
2022-09-21 15:01:49 -0700

    strncpy

--- 2-strncpy.c
+++ 2-strncpy.c
@@ -16,29 +16,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 
-	int j = 0;
+int j = 0;
 
-	for (j = 0; src[j] != '\0'; j++)
-
-	{
-
-		if (j < n)
-
-		{
-
-			dest[j] = src[j];
-
-		}
-
-	}
-
-	for (; j < n; j++)
-
-	{
-
-		dest[j] = '\0';
-
-	}
-
-	return (dest);
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < n)
+{
+dest[j] = src[j];
+}
+}
+for (; j < n; j++)
+{
+dest[j] = '\0';
+}
+return (dest);
 }
