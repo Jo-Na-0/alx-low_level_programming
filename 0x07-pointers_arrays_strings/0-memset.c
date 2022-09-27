af54397e18a95b6ac402943290c395be1f306990
am-afreemind <jmutebi001@gmail.com>
2022-09-27 12:40:18 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Date: Tue,Sep27,2022
+ *
+ * Author: Mutebi Jonathan
+ *
+ * Description: Function fills memory with a constant byte.
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+
+	while (i > n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
