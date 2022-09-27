93a0396ed68898870231e11099b6e4f2e172801e
am-afreemind <jmutebi001@gmail.com>
2022-09-27 16:04:36 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any of a set of bytes
+ *
+ * @s: source string
+ *
+ * @accept: accepted characters
+ *
+ * Return: the string since the first found accepted character
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, b;
+
+	while (s[i])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[i] == accept[b])
+			{
+				s += i;
+				return (s);
+			}
+
+			b++;
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
